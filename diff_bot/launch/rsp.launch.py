a30160519c75c7acc1def86c0b54bbe9a5c9885c
mazendahab <mazendahab74@gmail,com>
2024-04-22 22:09:11 +0200

    new file:   .vscode/c_cpp_properties.json new file:   .vscode/settings.json modified:   diff_bot/launch/rsp.launch.py new file:   diff_bot/launch/simulationlaunch.py new file:   diff_bot/test/test.urdf new file:   diff_bot/urdf/caster_wheel.xacro new file:   diff_bot/urdf/chassis.xacro new file:   diff_bot/urdf/inertial_macros.xacro new file:   diff_bot/urdf/properties.xacro deleted:    diff_bot/urdf/robot.urdf new file:   diff_bot/urdf/robot.urdf.xacro new file:   diff_bot/urdf/test.urdf new file:   diff_bot/urdf/wheels.xacro

--- diff_bot/launch/rsp.launch.py
+++ diff_bot/launch/rsp.launch.py
@@ -1,32 +1,42 @@
 import os
 
-from ament_index_python.packages import get_package_share_directory
+from ament_index_python.packages import get_package_share_directory 
 from launch import LaunchDescription
-from launch.substitutions import Command
-from launch_ros.actions import Node
+from launch.substitutions import Command 
+from launch_ros.actions import Node 
+
+import xacro 
 
 
 def generate_launch_description():
-    urdf_file_name = 'robot.urdf'
+    urdf_file_name = 'robot.urdf.xacro'
     package_name = "diff_bot"
-    ############ DATA INPUT END ############
+
     print("fetching urdf ==>")
-    ##path to the package##
+
     robot_desc_path = os.path.join(get_package_share_directory(package_name),"urdf",urdf_file_name)
+    xacro_file = xacro.process_file(robot_desc_path)
 
-    #robot state publisher#
-
-    urdf_content = open(robot_desc_path).read()
     robot_state_publisher_node =  Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher_node',
-        parameters=[{'use_sim_time': True, 'robot_description': urdf_content}],
+        parameters=[{'use_sim_time': True, 'robot_description': xacro_file.toxml()}],
+        output='screen')
+    
+    joint_state_publisher_node =  Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher_node',
+        parameters=[{'use_sim_time': True, 'robot_description': xacro_file.toxml()}],
         output='screen')
                
-    return LaunchDescription([
-                        robot_state_publisher_node
-    ])
+    return LaunchDescription(
+        [
+                        robot_state_publisher_node,
+                        joint_state_publisher_node
+        ]
+    )
     
     
     
